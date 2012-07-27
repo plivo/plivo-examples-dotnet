0812e3ef44a5e26fdf30eae58bba3a28fa250d40
Rakesh Kadarla <rakesh@plivo.com>
2012-07-27 11:01:47 -0700

    examples for using Plivo message resource

---
+++ PlivoMessage.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using RestSharp;
+using Plivo.API;
+
+namespace PlivoMessage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RestAPI plivo = new RestAPI("XXXXXXXXXXXXXXXXXXXXXXX", "YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY");
+
+            IRestResponse<Message> resp = plivo.send_message(new Dictionary<string, string>() 
+            {
+                { "src", "11212121211" },
+                { "dst", "12212121211" },
+                { "text", "Hi, text from Plivo." },
+                { "url", "http://some.domain/receivestatus/" },
+                { "method", "GET" }
+            });
+            if (resp.Data != null)
+            {
+                PropertyInfo[] proplist = resp.Data.GetType().GetProperties();
+                foreach (PropertyInfo property in proplist)
+                    Console.WriteLine("{0}: {1}", property.Name, property.GetValue(resp.Data, null));
+            }
+            else
+                Console.WriteLine(resp.ErrorMessage);
+		}
+	}
+}	
