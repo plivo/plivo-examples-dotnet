11cccc0eb9e3f0c41f9d469c494a8c9366e76c7c
Ramya <ramya@plivo.com>
2015-02-12 16:17:52 +0530

    send custom sip headers

--- voice/call_api/custom_sip_header.cs
+++ voice/call_api/custom_sip_header.cs
@@ -14,7 +14,7 @@ namespace custom_sip_header
             IRestResponse<Call> resp = plivo.make_call(new Dictionary<string, string>() 
             {
                 { "from", "1111111111" }, // The phone number to which the call has to be placed
-                { "to", "2222222222" }, // The phone number to be used as the caller Id
+                { "to", "sip:abcd150105094929@phone.plivo.com" }, // The phone number to be used as the caller Id
                 { "answer_url", "http://dotnettest.apphb.com/speak" }, // The URL invoked by Plivo when the outbound call is answered
                 {"answer_method","GET"}, // The method used to invoke the answer_url
                 {"sip_headers","Test=Sample"} // List of SIP Headers in the form of 'key=value' pairs, separated by commas 
@@ -35,4 +35,11 @@ namespace custom_sip_header
   "message": "call fired",
   "request_uuid": "93ef02ab-8a10-4eef-9127-2fcf0fe487ae"
 }
+
+The SIP header can be seen as a query parameter in the answer_url
+/speak?Direction=outbound&From=18583650866&ALegUUID=fad58e02-b297-11e4-96d7-377ffe01233f&BillRate=0.03570&To=919663489033&
+X-PH-Test=Sample&CallUUID=fad58e02-b297-11e4-96d7-377ffe01233f&ALegRequestUUID=93ef02ab-8a10-4eef-9127-2fcf0fe487ae&
+RequestUUID=93ef02ab-8a10-4eef-9127-2fcf0fe487ae&CallStatus=in-progress&Event=StartApp HTTP/1.1" 200 59 "-" 
+"Mozilla/5.0 (X11; Linux i686) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/14.0.835.35 Safari/535.1" 
+"source=nginx measure#http.response_time=0.088s measure#http.bytes.sent=217"
 */
