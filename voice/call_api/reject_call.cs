687534f1257b0560db2054e1a20d2f4b8631b4d0
Ramya <ramya@plivo.com>
2015-02-12 16:18:47 +0530

    reject calls using hangup xml

---
+++ voice/call_api/reject_call.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using RestSharp;
+using Plivo.XML;
+using Nancy;
+
+# Set the caller ID using Dial XML
+
+namespace hangup
+{
+    public class Program : NancyModule
+    {
+        public Program()
+        {
+            Get["/hangup"] = x =>
+            {
+                Plivo.XML.Response resp = new Plivo.XML.Response();
+                resp.AddSpeak("This call will be hung up in 1 minute", new Dictionary<string, string>(){});
+                resp.AddHangup(new Dictionary<string, string>() 
+                {
+                    {"reason","busy"}, // Specify the reason for hangup
+                    {"schedule","60"} // Schedule the hangup
+                });    
+
+                Debug.WriteLine(resp.ToString());
+
+                var output = resp.ToString();
+                var res = (Nancy.Response)output;
+                res.ContentType = "text/xml";
+                return res;
+            };
+
+        }
+    }
+}
+
+/*
+Sample Output
+<Response>
+    <Speak>This call will be hung up in 1 minute</Speak>
+    <Hangup reason="busy" schedule="60"/>
+</Response>
+*/
