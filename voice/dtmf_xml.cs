962080c6c98e655b50d906703a994628a8220780
Ramya <ramya@plivo.com>
2015-02-13 14:22:01 +0530

    DTMF XML example

---
+++ voice/dtmf_xml.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using RestSharp;
+using Plivo.XML;
+using Nancy;
+
+namespace custom_tone
+{
+    public class Program : NancyModule
+    {
+        public Program()
+        {
+            Get["/dtmf"] = x =>
+            {
+                Plivo.XML.Response resp = new Plivo.XML.Response();
+
+                // Add Speak XML Tag
+                resp.AddSpeak("Sending digits", new Dictionary<string, string>() { });
+
+                // Add DTMF XML Tag
+                resp.AddDTMF("12345", new Dictionary<string, string>() { });
+
+                Debug.WriteLine(resp.ToString());
+
+                var output = resp.ToString();
+                var res = (Nancy.Response)output;
+                res.ContentType = "text/xml";
+                return res;
+
+            };
+        }
+    }
+}
+
+/*
+Sample Output
+<Response>
+    <Speak>Sending Digits</Speak>
+    <DTMF>12345</DTMF>
+</Response>
+*/
