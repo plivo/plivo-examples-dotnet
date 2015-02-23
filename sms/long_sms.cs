43f9b652822605d870670171bcc2d1760eec715c
Ramya <ramya@plivo.com>
2015-02-23 17:35:09 +0530

    sms units added

--- sms/long_sms.cs
+++ sms/long_sms.cs
@@ -16,11 +16,30 @@ namespace Long_Sms
             {
                 { "src", "1111111111" }, // Sender's phone number with country code
                 { "dst", "2222222222" }, // Receiver's phone number wiht country code
-                { "text", "This randomly generated text can be used in your layout (webdesign , websites, books, posters ... ) for free. This text is entirely free of law. Feel free to link to this site by using the image below or by making a simple text link" } // Your SMS text message
+                // Your SMS text message - English
+                { "text", "This randomly generated text can be used in your layout (webdesign , websites, books, posters ... ) for free. This text is entirely free of law. Feel free to link to this site by using the image below or by making a simple text link" }
+                // Send uicode text
+                // Your SMS text message - Japanese
+                // {"text", "このランダムに生成されたテキストは、自由のためのあなたのレイアウト（ウェブデザイン、ウェブサイト、書籍、ポスター...）で使用することができます。このテキストは、法律の完全に無料です。下の画像を使用して、または単純なテキストリンクを作ることで、このサイトへのリンクフリーです"}
+                // Your SMS text message - French
+                // {"text", "Ce texte généré aléatoirement peut-être utilisé dans vos maquettes (webdesign, sites internet,livres, affiches...) gratuitement. Ce texte est entièrement libre de droit. N'hésitez pas à faire un lien sur ce site en utilisant l'image ci-dessous ou en faisant un simple lien texte}
             });
 
             //Prints the message details
             Console.Write(resp.Content);
+
+            // Get the details of the sent message
+            string uuid = resp.Data.message_uuid[0];
+
+            RestAPI plivo1 = new RestAPI("Your AUTH_ID", "Your AUTH_TOKEN");
+
+            IRestResponse<MessageResponse> response = plivo.send_message(new Dictionary<string, string>() 
+            {
+                { "record_id", uuid } // Message UUID
+            });
+
+            Console.WriteLine("Number of units : {0}", response.Data.units);
+            Console.ReadLine();
         }
     }
 }
@@ -33,4 +52,13 @@ namespace Long_Sms
    u'api_id': u'dce8fb42-9260-11e4-b932-22000ac50fac'
    }
 )
+
+For English
+Number of units : 2
+
+For Japanese
+Number of units : 3
+
+For French
+Number of units : 5
 */
